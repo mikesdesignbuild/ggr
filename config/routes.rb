a3df362666180bd1b038c681bfc511c605f68366
Mike Plusch <mikeplusch@gmail.com>
2013-12-26 14:05:39 -0500

    rails generate controller StaticPages home help

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ggr::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
