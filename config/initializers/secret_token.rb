0fc3b2844ce9906cc295a7516a4c4971c7e667a3
Mike Plusch <mikeplusch@gmail.com>
2013-12-26 10:18:23 -0500

    start with rails generate ggr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ggr::Application.config.secret_key_base = '6d4047188a336aab898d719bc59ebb7b629ea06b72d1c4ecab4f2cdca25b1937f208b57e7acbb2185605e5d9a110e9e2b207d794cd6928ed3066f2b2934229bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ggr::Application.config.session_store :cookie_store, key: '_ggr_session'
