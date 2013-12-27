9e62fca30ba49e1610ac79e6b36d245bb133fbb6
Mike Plusch <mikeplusch@gmail.com>
2013-12-27 13:54:12 -0500

    store current_user in cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   def set_current
     # session["current"]=@user TODO
-    cookies[:current]=@user.id
+    cookies[:current_user] = @user.id
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
