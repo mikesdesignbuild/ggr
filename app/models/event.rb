9f82aa779c98815e6f98df760b0785047711fcd0
Mike Plusch <mikeplusch@gmail.com>
2013-12-30 08:10:30 -0500

    FIX circular error by correctly renaming all files

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
- has_many :participations
- has_many :members, through: :participations
+  has_many :participations
+  has_many :members, through: :participations
+
 #    t.date     "on_date"
 #    t.time     "at_time"
 #    t.integer  "boat"
